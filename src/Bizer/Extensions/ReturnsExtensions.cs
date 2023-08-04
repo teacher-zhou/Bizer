72c105da9652d2f6af6bcb92739647e540be056f
Zhou Zihao <william-zzh@outlook.com>
2023-08-04 10:42:53 +0800

    更新 Returns 的 Log 日志扩展方法

--- src/Bizer/Extensions/ReturnsExtensions.cs
+++ src/Bizer/Extensions/ReturnsExtensions.cs
@@ -3,30 +3,149 @@
 namespace Bizer;
 public static class ReturnsExtensions
 {
-    static Returns LogAsReturns(this Returns instance, ILogger? logger,LogLevel level,Exception? exception=default)
+    static Returns LogAsReturns(this Returns instance, ILogger? logger, LogLevel level,string? logMessage=default, Exception? exception = default)
     {
-        logger?.Log(level, exception, instance.Messages.JoinAsString("；"));
+        logger?.Log(level, exception, message: logMessage ?? instance.Messages.JoinAsString("；"));
         return instance;
     }
-    static Returns<TResult> LogAsReturns<TResult>(this Returns<TResult> instance, ILogger? logger, LogLevel level, Exception? exception = default)
+    static Returns<TResult> LogAsReturns<TResult>(this Returns<TResult> instance, ILogger? logger, LogLevel level, string? logMessage = default, Exception? exception = default)
     {
-        logger?.Log(level, exception, instance.Messages.JoinAsString("；"));
+        logger?.Log(level, exception, message: logMessage ?? instance.Messages.JoinAsString("；"));
         return instance;
     }
 
+    /// <summary>
+    /// 表示返回失败的结果。
+    /// </summary>
+    /// <param name="instance"></param>
+    /// <param name="errors">错误消息的列表。</param>
+    /// <returns></returns>
     public static Returns Failed(this Returns instance, IEnumerable<string> errors) => instance.Failed(errors.ToArray());
-    public static Returns<TResult> Failed<TResult>(this Returns<TResult> instance, IEnumerable<string> errors) => instance.Failed(errors.ToArray());
+    /// <summary>
+    /// 表示返回失败的结果。
+    /// </summary>
+    /// <param name="instance"></param>
+    /// <param name="errors">错误消息的列表。</param>
+    public static Returns<TResult> Failed<TResult>(this Returns<TResult> instance, IEnumerable<string> errors) 
+        => instance.Failed(errors.ToArray());
 
-    public static Returns LogTrace(this Returns instance, ILogger? logger) => instance.LogAsReturns(logger, LogLevel.Trace);
-    public static Returns LogDebug(this Returns instance, ILogger? logger) => instance.LogAsReturns(logger, LogLevel.Debug);
-    public static Returns LogInfo(this Returns instance, ILogger? logger) => instance.LogAsReturns(logger, LogLevel.Information);
-    public static Returns LogWarning(this Returns instance, ILogger? logger) => instance.LogAsReturns(logger, LogLevel.Warning);
-    public static Returns LogError(this Returns instance, ILogger? logger, Exception? exception = default) => instance.LogAsReturns(logger, LogLevel.Error, exception);
-    public static Returns LogFatal(this Returns instance, ILogger? logger, Exception? exception = default) => instance.LogAsReturns(logger, LogLevel.Critical, exception);
-    public static Returns<TResult> LogTrace<TResult>(this Returns<TResult> instance, ILogger? logger) => instance.LogAsReturns(logger, LogLevel.Trace);
-    public static Returns<TResult> LogDebug<TResult>(this Returns<TResult> instance, ILogger? logger) => instance.LogAsReturns(logger, LogLevel.Debug);
-    public static Returns<TResult> LogInfo<TResult>( this Returns<TResult> instance, ILogger? logger) => instance.LogAsReturns(logger, LogLevel.Information);
-    public static Returns<TResult> LogWarning<TResult>(this Returns<TResult> instance, ILogger? logger) => instance.LogAsReturns(logger, LogLevel.Warning);
-    public static Returns<TResult> LogError<TResult>(this Returns<TResult> instance, ILogger? logger, Exception? exception = default) => instance.LogAsReturns(logger, LogLevel.Error, exception);
-    public static Returns<TResult> LogFatal<TResult>(this Returns<TResult> instance, ILogger? logger, Exception? exception = default) => instance.LogAsReturns(logger, LogLevel.Critical, exception);
+    /// <summary>
+    /// 记录日志是 TRACE 级别的日志。
+    /// </summary>
+    /// <param name="instance"></param>
+    /// <param name="logger">日志对象。</param>
+    /// <param name="logMessage">日志的消息。<c>null</c> 则使用 <see cref="Returns.Messages"/> 的值。</param>
+    /// <param name="exception">异常对象。</param>
+    /// <returns></returns>
+    public static Returns LogTrace(this Returns instance, ILogger? logger, string? logMessage = default, Exception? exception = default) 
+        => instance.LogAsReturns(logger, LogLevel.Trace, logMessage, exception);
+
+    /// <summary>
+    /// 记录日志是 DEBUG 级别的日志。
+    /// </summary>
+    /// <param name="instance"></param>
+    /// <param name="logger">日志对象。</param>
+    /// <param name="logMessage">日志的消息。<c>null</c> 则使用 <see cref="Returns.Messages"/> 的值。</param>
+    public static Returns LogDebug(this Returns instance, ILogger? logger, string? logMessage = default, Exception? exception = default) 
+        => instance.LogAsReturns(logger, LogLevel.Debug, logMessage, exception);
+
+    /// <summary>
+    /// 记录日志是 INFO 级别的日志。
+    /// </summary>
+    /// <param name="instance"></param>
+    /// <param name="logger">日志对象。</param>
+    /// <param name="logMessage">日志的消息。<c>null</c> 则使用 <see cref="Returns.Messages"/> 的值。</param>
+    /// <param name="exception">异常对象。</param>
+    public static Returns LogInfo(this Returns instance, ILogger? logger, string? logMessage = default, Exception? exception = default) 
+        => instance.LogAsReturns(logger, LogLevel.Information, logMessage, exception);
+
+    /// <summary>
+    /// 记录日志是 WARNING 级别的日志。
+    /// </summary>
+    /// <param name="instance"></param>
+    /// <param name="logger">日志对象。</param>
+    /// <param name="logMessage">日志的消息。<c>null</c> 则使用 <see cref="Returns.Messages"/> 的值。</param>
+    /// <param name="exception">异常对象。</param>
+    public static Returns LogWarning(this Returns instance, ILogger? logger, string? logMessage = default, Exception? exception = default) 
+        => instance.LogAsReturns(logger, LogLevel.Warning, logMessage, exception);
+
+    /// <summary>
+    /// 记录日志是 ERROR 级别的日志。
+    /// </summary>
+    /// <param name="instance"></param>
+    /// <param name="logger">日志对象。</param>
+    /// <param name="logMessage">日志的消息。<c>null</c> 则使用 <see cref="Returns.Messages"/> 的值。</param>
+    /// <param name="exception">异常对象。</param>
+    public static Returns LogError(this Returns instance, ILogger? logger, string? logMessage = default, Exception? exception = default) 
+        => instance.LogAsReturns(logger, LogLevel.Error, logMessage, exception);
+
+    /// <summary>
+    /// 记录日志是 FATAL 级别的日志。
+    /// </summary>
+    /// <param name="instance"></param>
+    /// <param name="logger">日志对象。</param>
+    /// <param name="logMessage">日志的消息。<c>null</c> 则使用 <see cref="Returns.Messages"/> 的值。</param>
+    /// <param name="exception">异常对象。</param>
+    public static Returns LogFatal(this Returns instance, ILogger? logger, string? logMessage = default, Exception? exception = default) 
+        => instance.LogAsReturns(logger, LogLevel.Critical, logMessage, exception);
+
+    /// <summary>
+    /// 记录日志是 TRACE 级别的日志。
+    /// </summary>
+    /// <param name="instance"></param>
+    /// <param name="logger">日志对象。</param>
+    /// <param name="logMessage">日志的消息。<c>null</c> 则使用 <see cref="Returns.Messages"/> 的值。</param>
+    /// <param name="exception">异常对象。</param>
+    public static Returns<TResult> LogTrace<TResult>(this Returns<TResult> instance, ILogger? logger, string? logMessage = default, Exception? exception = default) 
+        => instance.LogAsReturns(logger, LogLevel.Trace, logMessage, exception);
+
+    /// <summary>
+    /// 记录日志是 DEBUG 级别的日志。
+    /// </summary>
+    /// <param name="instance"></param>
+    /// <param name="logger">日志对象。</param>
+    /// <param name="logMessage">日志的消息。<c>null</c> 则使用 <see cref="Returns.Messages"/> 的值。</param>
+    /// <param name="exception">异常对象。</param>
+    public static Returns<TResult> LogDebug<TResult>(this Returns<TResult> instance, ILogger? logger, string? logMessage = default, Exception? exception = default) 
+        => instance.LogAsReturns(logger, LogLevel.Debug, logMessage, exception);
+
+    /// <summary>
+    /// 记录日志是 INFO 级别的日志。
+    /// </summary>
+    /// <param name="instance"></param>
+    /// <param name="logger">日志对象。</param>
+    /// <param name="logMessage">日志的消息。<c>null</c> 则使用 <see cref="Returns.Messages"/> 的值。</param>
+    /// <param name="exception">异常对象。</param>
+    public static Returns<TResult> LogInfo<TResult>( this Returns<TResult> instance, ILogger? logger, string? logMessage = default, Exception? exception = default) 
+        => instance.LogAsReturns(logger, LogLevel.Information, logMessage, exception);
+
+    /// <summary>
+    /// 记录日志是 WARNING 级别的日志。
+    /// </summary>
+    /// <param name="instance"></param>
+    /// <param name="logger">日志对象。</param>
+    /// <param name="logMessage">日志的消息。<c>null</c> 则使用 <see cref="Returns.Messages"/> 的值。</param>
+    /// <param name="exception">异常对象。</param>
+    public static Returns<TResult> LogWarning<TResult>(this Returns<TResult> instance, ILogger? logger, string? logMessage = default, Exception? exception = default) 
+        => instance.LogAsReturns(logger, LogLevel.Warning,logMessage, exception);
+
+    /// <summary>
+    /// 记录日志是 ERROR 级别的日志。
+    /// </summary>
+    /// <param name="instance"></param>
+    /// <param name="logger">日志对象。</param>
+    /// <param name="logMessage">日志的消息。<c>null</c> 则使用 <see cref="Returns.Messages"/> 的值。</param>
+    /// <param name="exception">异常对象。</param>
+    public static Returns<TResult> LogError<TResult>(this Returns<TResult> instance, ILogger? logger, string? logMessage = default, Exception? exception = default)
+        => instance.LogAsReturns(logger, LogLevel.Error, logMessage, exception);
+
+    /// <summary>
+    /// 记录日志是 FATAL 级别的日志。
+    /// </summary>
+    /// <param name="instance"></param>
+    /// <param name="logger">日志对象。</param>
+    /// <param name="logMessage">日志的消息。<c>null</c> 则使用 <see cref="Returns.Messages"/> 的值。</param>
+    /// <param name="exception">异常对象。</param>
+    public static Returns<TResult> LogFatal<TResult>(this Returns<TResult> instance, ILogger? logger, string? logMessage = default, Exception? exception = default)
+        => instance.LogAsReturns(logger, LogLevel.Critical, logMessage, exception);
 }
