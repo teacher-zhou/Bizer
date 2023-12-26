1baf2ce5e2ce2e7ed66e68f6366ec7e54a232430
Zhou Zihao <william-zzh@outlook.com>
2023-12-26 17:57:53 +0800

    搜索的例子

--- sample/Sample.Services/ITestService.cs
+++ sample/Sample.Services/ITestService.cs
@@ -1,40 +1,54 @@
 ﻿using Bizer;
+using Bizer.Services;
 
-namespace Sample.Services
+namespace Sample.Services;
+
+[ApiRoute("api/test")]
+public interface ITestService
 {
-    [ApiRoute("api/test")]
-    public interface ITestService
+    /// <summary>
+    /// 获取
+    /// </summary>
+    /// <returns></returns>
+    [Get]
+    Task<Returns> GetAsync();
+
+    [Get("{id}")]
+    Task<Returns> GetFromPathAsync([Path] int id);
+    [Get("query")]
+    Task<Returns> GetFromQueryAsync([Query] string? name);
+
+    [Get("header")]
+    Task<Returns> GetFromHeaderAsync([Header("Authorization")] string? header);
+
+    [Get("data")]
+    Task<Returns<string>> GetHasData();
+
+    [Post]
+    Task<string> PostAsync(string data);
+
+    [Post("no-return")]
+    Task PostNothingAsync();
+
+    [Put("{name}")]
+    Returns<string> PutData([Path] string name);
+
+    [Put("no-return")]
+    void PutNothing(int? id);
+    [Post("auth")]
+    Task Auth();
+
+    [Get("search")]
+    Task<Returns> Search([Query] SearchFilter model);
+}
+
+
+public record SearchFilter(int Page, int Size) : PagedDto(Page, Size)
+{
+    public SearchFilter() : this(1, 10)
     {
-        /// <summary>
-        /// 获取
-        /// </summary>
-        /// <returns></returns>
-        [Get]
-        Task<Returns> GetAsync();
 
-        [Get("{id}")]
-        Task<Returns> GetFromPathAsync([Path]int id);
-        [Get("query")]
-        Task<Returns> GetFromQueryAsync([Query]string? name);
-
-        [Get("header")]
-        Task<Returns> GetFromHeaderAsync([Header("Authorization")]string? header);
-
-        [Get("data")]
-        Task<Returns<string>> GetHasData();
-
-        [Post]
-        Task<string> PostAsync(string data);
-
-        [Post("no-return")]
-        Task PostNothingAsync();
-
-        [Put("{name}")]
-        Returns<string> PutData([Path]string name);
-
-        [Put("no-return")]
-        void PutNothing(int? id);
-        [Post("auth")]
-        Task Auth();
     }
+
+    public string? Title { get; set; }
 }
--- sample/Sample.WebApi/TestService.cs
+++ sample/Sample.WebApi/TestService.cs
@@ -1,62 +1,68 @@
 ﻿using Bizer;
+
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+
 using Sample.Services;
 
-namespace Sample.WebApi
+namespace Sample.WebApi;
+
+public class TestService : ITestService
 {
-    public class TestService : ITestService
+    [Authorize]
+    public Task Auth()
     {
-        [Authorize]
-        public Task Auth()
-        {
-            throw new NotImplementedException();
-        }
+        throw new NotImplementedException();
+    }
 
-        [ProducesResponseType(200,Type=typeof(Returns))]
-        public Task<Returns> GetAsync()
-        {
-            return Returns.Success().WithMessage("获取成功").ToResultTask();
-        }
+    [ProducesResponseType(200, Type = typeof(Returns))]
+    public Task<Returns> GetAsync()
+    {
+        return Returns.Success().WithMessage("获取成功").ToResultTask();
+    }
 
-        [ProducesResponseType(200, Type = typeof(Returns))]
-        public Task<Returns> GetFromHeaderAsync(string? header)
-        {
-            return Returns.Success().ToResultTask();
-        }
+    [ProducesResponseType(200, Type = typeof(Returns))]
+    public Task<Returns> GetFromHeaderAsync(string? header)
+    {
+        return Returns.Success().ToResultTask();
+    }
 
-        public Task<Returns> GetFromPathAsync(int id)
-        {
-            return Returns.Success().ToResultTask();
-        }
+    public Task<Returns> GetFromPathAsync(int id)
+    {
+        return Returns.Success().ToResultTask();
+    }
 
-        public Task<Returns> GetFromQueryAsync([Query(null)] string? name)
-        {
-            return Returns.Success().ToResultTask();
-        }
+    public Task<Returns> GetFromQueryAsync([Query(null)] string? name)
+    {
+        return Returns.Success().ToResultTask();
+    }
 
-        [ProducesResponseType(200, Type = typeof(Returns<string>))]
-        public Task<Returns<string>> GetHasData()
-        {
-            return Returns<string>.Success("返回值").WithCode("20003").ToResultTask();
-        }
+    [ProducesResponseType(200, Type = typeof(Returns<string>))]
+    public Task<Returns<string>> GetHasData()
+    {
+        return Returns<string>.Success("返回值").WithCode("20003").ToResultTask();
+    }
 
-        public Task<string> PostAsync(string data)
-        {
-            return "一般返回值".ToResultTask();
-        }
+    public Task<string> PostAsync(string data)
+    {
+        return "一般返回值".ToResultTask();
+    }
 
-        public Task PostNothingAsync()
-        {
-            return Task.CompletedTask;
-        }
-        public Returns<string> PutData(string name)
-        {
-            return Returns<string>.Success(name);
-        }
+    public Task PostNothingAsync()
+    {
+        return Task.CompletedTask;
+    }
+    public Returns<string> PutData(string name)
+    {
+        return Returns<string>.Success(name);
+    }
 
-        public void PutNothing(int? id)
-        {
-        }
+    public void PutNothing(int? id)
+    {
+    }
+
+    public Task<Returns> Search([Query] SearchFilter model)
+    {
+        return Returns.Success().ToResultTask();
     }
 }
